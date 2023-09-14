bde25b04fd7ed28648cf26bec4a549b20a7620d0
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-09-14 11:40:27 +0000

    Update 3 files

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,38 +1,37 @@
 export default function Login() {
   return (
-    <main className="container">
-      <div className="row justify-content-center align-items-center vh-100">
-        <div className="row justify-content-center align-items-center vh-100">
-          <form className="login-form d-flex flex-column gap-2" action="">
-            <div className="form-group row">
-              <label className="col-sm-4 col-form-label" htmlFor="">
-                Username
-              </label>
-              <input
-                className="col-sm-8"
-                type="text"
-                name="username"
-                id="username"
-              />
-            </div>
-            <div className="form-group row">
-              <label className="col-sm-4 col-form-label" htmlFor="">
-                Password
-              </label>
-              <input
-                className="col-sm-8"
-                type="text"
-                name="password"
-                id="password"
-              />
-            </div>
-            <div class="mb-3 text-center">
-              <button type="submit" class="btn btn-primary">
-                Login
-              </button>
-            </div>
-          </form>
-        </div>
+    <main className="row justify-content-center align-items-center vh-100">
+      <div className="col-sm-6 shadow rounded bg-white p-4">
+        <h2 class="mb-3">Log In</h2>
+        <form className="login-form d-flex flex-column gap-2" action="">
+          <div className="form-group row">
+            <label className="col-sm-4 col-form-label" htmlFor="">
+              Username
+            </label>
+            <input
+              className="col-sm-6"
+              type="text"
+              name="username"
+              id="username"
+            />
+          </div>
+          <div className="form-group row">
+            <label className="col-sm-4 col-form-label" htmlFor="">
+              Password
+            </label>
+            <input
+              className="col-sm-6"
+              type="text"
+              name="password"
+              id="password"
+            />
+          </div>
+          <div class="mb-3 text-center">
+            <button type="submit" class="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </main>
   );
